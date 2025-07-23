ffc3eaf7cddc5a52e68a3fb3e17ce9b474ddb0cf
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-07-23 17:34:39 +0700

    feat: add capitalizeWords function

---
+++ lib/utils/string_utils.dart
@@ -0,0 +1,10 @@
+String capitalizeWords(String text) {
+  return text
+      .split(' ')
+      .map(
+        (word) => word.isEmpty
+            ? ''
+            : '${word[0].toUpperCase()}${word.substring(1).toLowerCase()}',
+      )
+      .join(' ');
+}
