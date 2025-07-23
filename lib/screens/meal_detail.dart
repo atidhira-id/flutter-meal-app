be711f4edc7331ff19921df4fbf41063b22395ea
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-07-23 17:36:32 +0700

    feat: add meal detail screen

---
+++ lib/screens/meal_detail.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/models/meal.dart';
+import 'package:meal_app/utils/string_utils.dart';
+import 'package:meal_app/widgets/meal_trait.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealDetail extends StatelessWidget {
+  const MealDetail({super.key, required this.meal});
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actionsPadding: EdgeInsets.symmetric(horizontal: 4),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.favorite)),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 8,
+                  children: [
+                    Text(
+                      meal.title,
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    Row(
+                      spacing: 8,
+                      children: [
+                        MealTrait(
+                          text: '${meal.duration} min',
+                          icon: Icons.schedule,
+                        ),
+                        MealTrait(
+                          text: capitalizeWords(meal.complexity.name),
+                          icon: Icons.work,
+                        ),
+                        MealTrait(
+                          text: capitalizeWords(meal.affordability.name),
+                          icon: Icons.attach_money,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.red[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: MealTrait(text: "4,5", icon: Icons.star),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          ShaderMask(
+            shaderCallback: (rect) {
+              return LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.transparent,
+                  Colors.white,
+                  Colors.white,
+                  Colors.transparent,
+                ],
+                stops: [0.0, 0.3, 0.7, 1.0],
+              ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+            },
+            blendMode: BlendMode.dstIn,
+            child: FadeInImage(
+              width: double.infinity,
+              height: 280,
+              fit: BoxFit.cover,
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18),
+            child: Column(
+              children: [
+                Text(
+                  "Ingredients",
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
