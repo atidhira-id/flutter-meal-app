7da23a8e038bb590ac18e0e6e1e92a7c4e3f5e07
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-07-23 17:36:15 +0700

    feat: add meal trait, fadeInImage

--- lib/widgets/meal_list_item.dart
+++ lib/widgets/meal_list_item.dart
@@ -1,60 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app/models/meal.dart';
+import 'package:meal_app/utils/string_utils.dart';
+import 'package:meal_app/widgets/meal_trait.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MealListItem extends StatelessWidget {
-  const MealListItem({super.key, required this.meal});
+  const MealListItem({
+    super.key,
+    required this.meal,
+    required this.onMealSelect,
+  });
 
   final Meal meal;
+  final void Function() onMealSelect;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 40),
-      child: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 200,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(meal.imageUrl),
-                fit: BoxFit.cover,
+      child: InkWell(
+        onTap: onMealSelect,
+        splashColor: Theme.of(context).primaryColor,
+        child: Stack(
+          children: [
+            FadeInImage(
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: const Color.fromARGB(180, 0, 0, 0),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 18,
+                  vertical: 8,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 6),
+                    Row(
+                      spacing: 10,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealTrait(
+                          text: '${meal.duration} min',
+                          icon: Icons.schedule,
+                        ),
+                        MealTrait(
+                          text: capitalizeWords(meal.complexity.name),
+                          icon: Icons.work,
+                        ),
+                        MealTrait(
+                          text: capitalizeWords(meal.affordability.name),
+                          icon: Icons.attach_money_sharp,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              color: const Color.fromARGB(180, 0, 0, 0),
-              padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 8),
-              child: Column(
-                children: [
-                  whiteText(meal.title, 16, FontWeight.bold),
-                  const SizedBox(height: 6),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const SizedBox(),
-                      whiteText(meal.complexity.name, 12, FontWeight.normal),
-                      whiteText(meal.affordability.name, 12, FontWeight.normal),
-                      const SizedBox(),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
-    );
-  }
-
-  Text whiteText(String text, double size, FontWeight weight) {
-    return Text(
-      text,
-      style: TextStyle(fontSize: size, fontWeight: weight, color: Colors.white),
     );
   }
 }
