5be35de07f9711d08ede9659242fa92ee413751d
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-07-27 20:54:13 +0700

    feat: add ingredients and steps widget

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -18,90 +18,155 @@ class MealDetail extends StatelessWidget {
           IconButton(onPressed: () {}, icon: const Icon(Icons.favorite)),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  spacing: 8,
-                  children: [
-                    Text(
-                      meal.title,
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                    ),
-                    Row(
-                      spacing: 8,
-                      children: [
-                        MealTrait(
-                          text: '${meal.duration} min',
-                          icon: Icons.schedule,
-                        ),
-                        MealTrait(
-                          text: capitalizeWords(meal.complexity.name),
-                          icon: Icons.work,
-                        ),
-                        MealTrait(
-                          text: capitalizeWords(meal.affordability.name),
-                          icon: Icons.attach_money,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                  decoration: BoxDecoration(
-                    color: Colors.red[300],
-                    borderRadius: BorderRadius.circular(10),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 8,
+                    children: [
+                      Text(
+                        meal.title,
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      Row(
+                        spacing: 8,
+                        children: [
+                          MealTrait(
+                            text: '${meal.duration} min',
+                            icon: Icons.schedule,
+                          ),
+                          MealTrait(
+                            text: capitalizeWords(meal.complexity.name),
+                            icon: Icons.work,
+                          ),
+                          MealTrait(
+                            text: capitalizeWords(meal.affordability.name),
+                            icon: Icons.attach_money,
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  child: MealTrait(text: "4,5", icon: Icons.star),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 20),
-          ShaderMask(
-            shaderCallback: (rect) {
-              return LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Colors.transparent,
-                  Colors.white,
-                  Colors.white,
-                  Colors.transparent,
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.red[300],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: MealTrait(text: "4,5", icon: Icons.star),
+                  ),
                 ],
-                stops: [0.0, 0.3, 0.7, 1.0],
-              ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
-            },
-            blendMode: BlendMode.dstIn,
-            child: FadeInImage(
-              width: double.infinity,
-              height: 280,
-              fit: BoxFit.cover,
-              placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(meal.imageUrl),
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18),
-            child: Column(
-              children: [
-                Text(
-                  "Ingredients",
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
-              ],
+            const SizedBox(height: 20),
+            ShaderMask(
+              shaderCallback: (rect) {
+                return LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.transparent,
+                    Colors.white,
+                    Colors.white,
+                    Colors.transparent,
+                  ],
+                  stops: [0.0, 0.3, 0.7, 1.0],
+                ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+              },
+              blendMode: BlendMode.dstIn,
+              child: FadeInImage(
+                width: double.infinity,
+                height: 280,
+                fit: BoxFit.cover,
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(meal.imageUrl),
+              ),
             ),
-          ),
-        ],
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Ingredients",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  Wrap(
+                    spacing: 8,
+                    runSpacing: 8,
+                    children: [
+                      for (final ingredient in meal.ingredients)
+                        SizedBox(
+                          width:
+                              (MediaQuery.of(context).size.width -
+                                  (18 * 2) -
+                                  8) /
+                              2,
+                          child: Text(
+                            ingredient,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Steps",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 16,
+                    children: [
+                      for (final step in meal.steps)
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('- ', style: TextStyle(color: Colors.white)),
+                            Expanded(
+                              child: Text(
+                                step,
+                                style: TextStyle(color: Colors.white),
+                                softWrap: true,
+                              ),
+                            ),
+                          ],
+                        ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 80),
+          ],
+        ),
       ),
     );
   }
