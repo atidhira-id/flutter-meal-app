f0ff188dd28649030585f62b7e4547ff28e31320
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-07-31 01:14:36 +0700

    feat: add availableMeals property. storing filtered meals based on filter

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,10 +6,12 @@ import 'package:meal_app/screens/meals_screen.dart';
 import 'package:meal_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  const CategoriesScreen({super.key, required this.availableMeals});
+
+  final List<Meal> availableMeals;
 
   void _goToCategory(BuildContext context, Category category) {
-    final List<Meal> meals = dummyMeal
+    final List<Meal> meals = availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
