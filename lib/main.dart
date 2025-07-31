7f589ea8e9b332feaf9382799dc240fccd9d52d3
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-07-31 22:02:17 +0700

    feat: wrap entire app with ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:meal_app/screens/tabs.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const ProviderScope(child: App()));
 }
 
 class App extends StatelessWidget {
